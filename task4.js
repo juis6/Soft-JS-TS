bd19c32484af90be64e56d706e50e9ad02dcffe5
juis6 <olegzozulia129@gmail.com>
2025-06-19 16:36:06 +0300

    task4.js

--- task4.js
+++ task4.js
@@ -7,4 +7,11 @@
 // Реалізуйте функцію submitHandler, яка буде викликатись при натисканні кнопки Submit.
 // При натисканні на кнопку відправки форми в консоль ма.ть вивестись введені дані з усіх полів.
 
-function submitHandler() {}
+function submitHandler() {
+    const form = document.forms.userForm;
+    
+    console.log(form.userName.value);
+    console.log(form.phone.value);
+    console.log(form.birthday.value);
+    console.log(form.email.value);
+}
