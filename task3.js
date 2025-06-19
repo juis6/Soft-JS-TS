34e83ab0ff1ba7f8022fe5ae71b68cce39138fd4
juis6 <olegzozulia129@gmail.com>
2025-06-19 16:33:54 +0300

    task3.js

--- task3.js
+++ task3.js
@@ -9,3 +9,18 @@
 // Цю розмітку потрібно вставити у існуючий html елемент із класом "create_elements"
 // Спробуйте не використовувати рядок HTML-коду для вставки (`innerHTML`).
 // Використовуйте методи DOM: `createElement()`, `appendChild()`, `setAttribute()`, `classList.add()` тощо.
+
+const container = document.querySelector('.create_elements');
+
+const main = document.createElement('main');
+main.classList.add('mainClass', 'check', 'item');
+
+const div = document.createElement('div');
+div.setAttribute('id', 'myDiv');
+
+const p = document.createElement('p');
+p.textContent = 'First paragraph';
+
+div.appendChild(p);
+main.appendChild(div);
+container.appendChild(main);
