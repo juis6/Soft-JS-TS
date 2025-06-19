f5e1d24084c71dde6ad1ccec37f9fd2dff51a62f
juis6 <olegzozulia129@gmail.com>
2025-06-19 16:23:27 +0300

    task1.js

--- task1.js
+++ task1.js
@@ -4,3 +4,26 @@
 // *Додатково: кількість елементів може бути довільною і невідомою наперед
 // *Додатково: на сторінці можуть бути інші списки, всередині потрібного списку можуть бути інші елементи, крім <li>.
 // Вважайте, що скрипт підключено в кінці body, тести не генерують подію `DOMContentLoaded`.
+
+const list = document.getElementById('list');
+const listItems = list.getElementsByTagName('li');
+
+if (listItems.length > 0) {
+    console.log(listItems[0].textContent);
+}
+
+if (listItems.length > 0) {
+    console.log(listItems[listItems.length - 1].textContent);
+}
+
+if (listItems.length > 1) {
+    console.log(listItems[1].textContent);
+}
+
+if (listItems.length > 2) {
+    console.log(listItems[2].textContent);
+}
+
+if (listItems.length > 3) {
+    console.log(listItems[3].textContent);
+}
